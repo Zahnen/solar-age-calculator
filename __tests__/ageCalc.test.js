fddadf9450549db1bf7a4a8333c2c4fd2ca2d147
Zahnen Garner <zahnen@gmail.com>
2020-11-13 08:50:58 -0800

    add plain english specs to test

---
+++ __tests__/ageCalc.test.js
@@ -0,0 +1,45 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+Describe: User Constructor
+Test: "Constructor should create a user object with name and age properties"
+Expect: (user.name).toEqual("Zahnen")
+Expect: (user.age).toEqual(28)
+
+Describe: mercuryCalc
+Test: "It should convert the user's age from earth years to Mercury years and return the result"
+Expect: ((user.age).mercCalc).toEqual(116.66)
+
+Describe: venusCalc
+Test: "It should convert the user's age from earth years to Mercury years and return the result"
+Expect: ((user.age).venusCalc).toEqual(45.16)
+
+Describe: marsCalc
+Test: "It should convert the user's age from earth years to Mercury years and return the result"
+Expect: ((user.age).marsCalc).toEqual(14.89)
+
+Describe: jupiterCalc
+Test: "It should convert the user's age from earth years to Mercury years and return the result"
+Expect: ((user.age).jupiterCalc).toEqual(2.36)
+
+Describe: expectCalc
+Test: "It should determine life expectancy by subtracting user age from 72.2 and return the result"
+Expect: ((user.age).expectCalc).toEqual(44.2)
+
+Test: "It should display negative numbers as positive numbers if user age is greater than life expectancy and return the result"
+Expect: ((user.age = 80).expectCalc).toEqual(7.8)
+*/
